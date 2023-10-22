5e249f00dc7a3650dda12bef2ed7a186d1444b38
Oscar Eriksson <oscaredvarderiksson@gmail.com>
2023-10-22 14:36:33 +0300

    Added material information to hittable.h and sphere.h

--- sphere.h
+++ sphere.h
@@ -6,7 +6,7 @@
 
 class sphere : public hittable {
     public:
-        sphere(point3 _center, double _radius) : center(_center), radius(_radius) {}
+        sphere(point3 _center, double _radius, shared_ptr<material> _material) : center(_center), radius(_radius), mat(_material) {}
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc = r.origin() - center;
@@ -31,6 +31,7 @@ class sphere : public hittable {
             vec3 outward_normal = (rec.p - center) / radius;
             rec.set_face_normal(r, outward_normal);
             rec.normal = (rec.p - center) / radius;
+            rec.mat = mat;
 
             return true;
         }
@@ -38,6 +39,7 @@ class sphere : public hittable {
     private:
         point3 center;
         double radius;
+        shared_ptr<material> mat;
 };
 
 #endif
